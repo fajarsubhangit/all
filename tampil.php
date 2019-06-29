5b8282394efe5993e9f0aeadabdcdf79e0531c62
fajar <fajarsubhan60@gmail.com>
2019-06-29 21:46:55 +0700

    menampilkan data dari file xml

--- tampil.php
+++ tampil.php
@@ -5,6 +5,16 @@ if(!$load) {
   echo "file tidak ada";
 }
 else {
-  echo "file ada";
+  echo "<h1>Data Buku</h1>";
+  foreach($load as $key => $value) {
+      echo "nama :". $value->id ."<br>";
+      echo "string :". $value->author."<br>";
+      echo "title :". $value->title."<br>";
+      echo "genre :".$value->genre."<br>";
+      echo "price :".$value->price."<br>";
+      echo "publish_date :".$value->publish_date."<br>";
+      echo "description :".$value->description."<br>";
+      echo "<hr>";
+  }
 }
  ?>
