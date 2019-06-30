c73f2f92a5b642e8c812b0bd5bb87dbd72a9a92f
fajar <fajarsubhan60@gmail.com>
2019-06-30 19:39:30 +0700

    update data json

--- tampil.php
+++ tampil.php
@@ -1,35 +1,3 @@
 <?php
-$data_buku = array(
-  "catalog" => [
-      "book" => [
-          [
-            "id"     => "bk101",
-            "author" => "Gambardella, Matthew",
-            "genre"  => "Computer"
-          ],
-          [
-            "id"     => "bk102",
-            "author" => "Ralls, Kim",
-            "genre"  => "Mignight Rain"
-          ]
-        ],
-      "catalog" => [
-          "catalog1" => [
-              "nama" => "shoppe",
-              "url"  => "https://shoppe.com"
-            ],
-          "catalog2" => [
-              "nama" => "buakalapak",
-              "url"  => "https://bukalapak.com"
-            ],
-          "catalog3" => [
-              "nama" => "tokopedia",
-              "url"  => "https://tokopedia.com"
-            ]
-        ],
-      "secure" => true
-    ]
-);
-
-
+  $xml = simplexml
  ?>
