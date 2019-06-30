42b20e22bcddc7aeb6583db652fcf6a332ddde28
fajar <fajarsubhan60@gmail.com>
2019-06-30 15:11:22 +0700

    update data.json and tampil.php

--- tampil.php
+++ tampil.php
@@ -1,13 +1,45 @@
 <?php
-$file_json = file_get_contents("data.json");
+$link = mysqli_connect("localhost","root","4dm1n242","test1");
+mysqli_set_charset($link,"UTF-8");
+$query = "SELECT * FROM belajar1";
+$result = mysqli_query($link,$query);
 
-$decode = json_decode($file_json,true);
+while($data=mysqli_fetch_assoc($result)) {
+  $json["site_options"]["option"] = $data["options"];
+}
+
+$database = array(
+  "host" => "localhost",
+  "user" => [
+      "admin" => "admin242",
+      "staff" => "staff242"
+    ],
+  "password" => false,
+  "db" => "test"
+);
 
 echo "<pre>";
-print_r($decode);
+print_r($database);
 echo "</pre>";
 
-foreach($decode as $key) {
-  echo $key[0]["id"];
-}
+echo "<hr>";
+echo "<br>";
+
+$encode = json_encode($database,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+echo "<pre>";
+echo $encode;
+echo "</pre>";
  ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+
+   </body>
+   <script>
+    
+   </script>
+ </html>
