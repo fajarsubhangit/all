f26b7500809ad4d345b094828f87c4d3c5702eec
fajar <fajarsubhan60@gmail.com>
2019-06-30 23:28:30 +0700

    update

--- ajax.js
+++ ajax.js
@@ -1,14 +1,13 @@
 var button = document.getElementById("click");
-var hasil = document.getElementById("hasil");
 
 function newAjax() {
   var request = new XMLHttpRequest();
-      request.open("POST","tampil.php",true);
+      request.open("POST","json.js",true);
       request.setRequestHeader("Content-type","applications/x-www-form-urlencoded");
       request.send();
       request.onreadystatechange = function() {
         if(request.readyState === 4 && request.status === 200) {
-          hasil.innerHTML = request.responseText;
+          alert(JSON.stringify(object,["nama"]));
         }
     }
 }
