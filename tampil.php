0791eb2552b79b15827aaea1d815a64d23b5a028
fajar <fajarsubhan60@gmail.com>
2019-07-01 14:26:04 +0700

    baru

--- tampil.php
+++ tampil.php
@@ -1,18 +1,11 @@
-<?php
-$link = mysqli_connect("localhost","root","4dm1n242","test1");
-$query  = "SELECT * FROM Artikel WHERE id =1 ";
-$result = mysqli_query($link,$query);
-
-$array = mysqli_fetch_assoc($result);
-
-$encode = json_encode($array);
-echo json_last_error_msg();
-echo "<p>ARRAY</p>";
-echo "<pre>";print_r($array);echo "</pre>";
-echo "<hr>";
-echo "JSON";
-echo "<pre>";
-echo $encode;
-echo "</pre>";
-
- ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p id="hasil"></p>
+    <script src="ajax.js"></script>
+  </body>
+</html>
